b581c48f063726bc3eed1c90c5aa7d6e1ba493f1
Aleksands Chernavin <djoreh64@yandex.ru>
2023-09-16 13:17:33 +0300

    bug fixes

--- script/script.js
+++ script/script.js
@@ -4,177 +4,195 @@ const cells = document.querySelectorAll('.cell');
 const background = document.querySelector('main');
 const container = document.querySelector('.container');
 const restartBtn = document.querySelector('.restart');
-const counter = document.querySelector('.counter')
-const cursorRed = document.querySelector('.cursor-red')
-const cursorBlue = document.querySelector('.cursor-blue')
-let popup = ''
+const counter = document.querySelector('.counter');
+const cursorRed = document.querySelector('.cursor-red');
+const cursorBlue = document.querySelector('.cursor-blue');
+let popup;
+let firstPlayer = true;
 let redPoints = 0;
 let bluePoints = 0;
-let redPointsCounter = document.querySelector('.red_points')
-let bluePointsCounter = document.querySelector('.blue_points')
-let firstPlayer = true;
-const clickAudio = document.querySelector('.click_audio')
-const restartAudio = document.querySelector('.restart_audio')
+let redPointsCounter = document.querySelector('.red_points');
+let bluePointsCounter = document.querySelector('.blue_points');
+const clickAudio = document.querySelector('.click_audio');
+const restartAudio = document.querySelector('.restart_audio');
 const winCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
-    [0, 4, 8], [2, 4, 6]             
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
-cursorRed.style.cssText = 'animation: cursorRedShake 1s infinite alternate'
-cursorBlue.style.cssText = 'animation: cursorBlueShake 1s infinite alternate'
-
 document.addEventListener('mousemove', (e) => {
-    let x = e.clientX
-    let y = e.clientY
-    cursorRed.animate({
-        top: `${y}px`,
-        left: `${x}px`
-    }, {duration: 1000, fill: 'forwards'}
-    )
-    cursorBlue.animate({
-        top: `${y}px`,
-        left: `${x}px`
-    }, {duration: 1000, fill: 'forwards'})
-})
-
-cells.forEach(cell => {
-    cell.addEventListener('click', () => {
-        if (!cell.innerHTML) {
-            clickAudioPlay()
-            backgroundChange();
-            if (firstPlayer) {
-                cell.innerHTML = '<span class=\'cell_text\'>Х</span>';
-                checkWin('Х');
-                firstPlayer = false;
-            } else  {
-                cell.innerHTML = '<span class=\'cell_text\'>O</span>';
-                checkWin('O');
-                firstPlayer = true;
-            }
-            if (isBoardFull() && !checkWin('Х') && !checkWin('O')) {
-                showPopup('Ничья!');
-                setTimeout(() => {restartAudio.play()}, 1000);
-                setTimeout(restart, 2000)
-            }
-        }
-    });
+  let x = e.clientX;
+  let y = e.clientY;
+  cursorRed.animate(
+    {
+      top: `${y}px`,
+      left: `${x}px`,
+    },
+    { duration: 1000, fill: 'forwards' }
+  );
+  cursorBlue.animate(
+    {
+      top: `${y}px`,
+      left: `${x}px`,
+    },
+    { duration: 1000, fill: 'forwards' }
+  );
 });
 
-function backgroundChange() {
-    cursorRed.classList.toggle('hide')
-    cursorBlue.classList.toggle('hide')
-    counter.classList.toggle('zero_theme_counter')
-    background.classList.toggle('zero_theme');
-    container.classList.toggle('zero_theme');
-    restartBtn.classList.toggle('zero_theme_btn');
-    cells.forEach(cell => {
-        cell.classList.toggle('zero_theme_cells');
-    });
-}
-
-function restartAudioPlay () {
-    if (redPoints !== 0 || bluePoints !== 0) {
-        restartAudio.play()
-        restartAudio.currentTime = 0
+cells.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    if (!cell.innerHTML) {
+      clickAudioPlay();
+      themeChange();
+      if (firstPlayer) {
+        cell.innerHTML = '<span class=\'cell_text\'>Х</span>';
+        if (checkWin('Х')) {
+          showPopup('Победили крестики!');
+          setTimeout(restartAudioPlay, 1000);
+          setTimeout(resetBoard, 2000);
+          redPoints += 1;
+          updateRedPoints();
+        } else if (isBoardFull()) {
+          showPopup('Ничья!');
+          setTimeout(restartAudioPlay, 1000);
+          setTimeout(resetBoard, 2000);
+        }
+        firstPlayer = false;
+      } else {
+        cell.innerHTML = '<span class=\'cell_text\'>O</span>';
+        if (checkWin('O')) {
+          showPopup('Победили нолики!');
+          setTimeout(restartAudioPlay, 1000);
+          setTimeout(resetBoard, 2000);
+          bluePoints += 1;
+          updateBluePoints();
+        } else if (isBoardFull()) {
+          showPopup('Ничья!');
+          setTimeout(restartAudioPlay, 1000);
+          setTimeout(resetBoard, 2000);
+        }
+        firstPlayer = true;
+      }
     }
+  });
+});
+
+function themeChange() {
+  cursorRed.classList.toggle('hide');
+  cursorBlue.classList.toggle('hide');
+  counter.classList.toggle('zero_theme_counter');
+  background.classList.toggle('zero_theme');
+  container.classList.toggle('zero_theme');
+  restartBtn.classList.toggle('zero_theme_btn');
+  cells.forEach((cell) => {
+    cell.classList.toggle('zero_theme_cells');
+  });
 }
 
-function clickAudioPlay () {
-    clickAudio.play()
-    clickAudio.currentTime = 0
+function restartAudioPlay() {
+  if (redPoints !== 0 || bluePoints !== 0) {
+    restartAudio.play();
+    restartAudio.currentTime = 0;
+  }
+}
+
+function clickAudioPlay() {
+  clickAudio.play();
+  clickAudio.currentTime = 0;
 }
 
 restartBtn.addEventListener('click', () => {
-        restartAudioPlay()
-        redPoints = 0
-        redPointsCounter.innerText = `${redPoints}`
-        bluePoints = 0
-        bluePointsCounter.innerText = `${bluePoints}`
-        restart();
+  restartAudioPlay();
+  redPoints = 0;
+  redPointsCounter.innerText = `${redPoints}`;
+  bluePoints = 0;
+  bluePointsCounter.innerText = `${bluePoints}`;
+  resetBoard();
 });
 
 document.addEventListener('keydown', (e) => {
-    if (e.code === 'KeyR') {
-        restart();
-        restartAudioPlay()
-        redPoints = 0
-        redPointsCounter.innerText = `${redPoints}`
-        bluePoints = 0
-        bluePointsCounter.innerText = `${bluePoints}`
-    }
+  if (e.code === 'KeyR') {
+    resetBoard();
+    restartAudioPlay();
+    redPoints = 0;
+    redPointsCounter.innerText = `${redPoints}`;
+    bluePoints = 0;
+    bluePointsCounter.innerText = `${bluePoints}`;
+  }
 });
 
-function animateText () {
-    let cellText = document.querySelectorAll('.cell_text')
-    cellText.forEach(text => {
-        text.style.cssText = 'transform: translateY(200px); animation: cellTextOut 1s'
-        setTimeout(() => {text.remove()}, 1000)
-    }) 
-    cells.forEach(cell => {
-        cell.classList.remove('zero_theme_cells')
-    });
+function animateText() {
+  const cellText = document.querySelectorAll('.cell_text');
+  cellText.forEach((text) => {
+    text.style.cssText = 'transform: translateY(200px); animation: cellTextOut 1s';
+    setTimeout(() => {
+      text.remove();
+    }, 1000);
+  });
+  cells.forEach((cell) => {
+    cell.classList.remove('zero_theme_cells');
+  });
 }
 
 function isBoardFull() {
-    return Array.from(cells).every(cell => cell.innerHTML !== '');
+  return Array.from(cells).every((cell) => cell.innerHTML !== '');
 }
 
-function restart() {
-    cells.forEach(cell => {
-        setTimeout(() => {cell.style.cssText = 'pointer-events: all'}, 1000)
-    })
-    cursorBlue.classList.add('hide')
-    cursorRed.classList.remove('hide')
-    firstPlayer = true;
-    counter.classList.remove('zero_theme_counter')
-    background.classList.remove('zero_theme');
-    container.classList.remove('zero_theme');
-    restartBtn.classList.remove('zero_theme_btn');
-    animateText()
+function resetBoard() {
+  cells.forEach((cell) => {
+    setTimeout(() => {
+      cell.style.cssText = 'pointer-events: all';
+    }, 1000);
+  });
+  cursorBlue.classList.add('hide');
+  cursorRed.classList.remove('hide');
+  firstPlayer = true;
+  counter.classList.remove('zero_theme_counter');
+  background.classList.remove('zero_theme');
+  container.classList.remove('zero_theme');
+  restartBtn.classList.remove('zero_theme_btn');
+  animateText();
 }
-
 
 function checkWin(p) {
-    const isWin = winCombinations.some(combination => {
-        return combination.every(i => {
-            return cells[i].innerHTML.includes(p);
-        });
+  const isWin = winCombinations.some((combination) => {
+    return combination.every((i) => {
+      return cells[i].innerHTML.includes(p);
     });
+  });
 
-    if (isWin) {
-        setTimeout(() => {restartAudioPlay()}, 1000);
-        setTimeout(restart, 2000);
-        cells.forEach(cell => {
-            cell.style.cssText = 'pointer-events: none'
-        })
-        popup = document.createElement('div');
-        popup.classList.add('popup');
-        background.appendChild(popup);
-        setTimeout(() => {
-            popup.remove();
-        }, 4000);
-        
-        if (p === 'Х') {
-            redPoints++;
-            redPointsCounter.innerText = `${redPoints}`;
-            popup.innerText = 'Победили крестики!';
-        } else {
-            bluePoints++;
-            bluePointsCounter.innerText = `${bluePoints}`;
-            popup.innerText = 'Победили нолики!';
-        }
-        return true;
-    }
-    return false;
+  if (isWin) {
+    cells.forEach((cell) => {
+      cell.style.cssText = 'pointer-events: none';
+    });
+    return true;
+  }
+  return false;
 }
 
 function showPopup(text) {
-    const popup = document.createElement('div');
-    popup.classList.add('popup');
+  if (!popup) {
+    popup = document.createElement('div');
     popup.innerText = text;
+    popup.classList.add('popup');
     background.appendChild(popup);
     setTimeout(() => {
-        popup.remove();
+      popup.remove();
+      popup = '';
     }, 4000);
+  }
+}
+
+function updateRedPoints() {
+  redPointsCounter.innerText = `${redPoints}`;
+}
+
+function updateBluePoints() {
+  bluePointsCounter.innerText = `${bluePoints}`;
 }
